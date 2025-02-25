881072b93b614a8a389cae7662fda93a4ee41342
AntonVyhovskyi <vygovskijanton1@gmail.com>
2025-02-25 19:45:00 +0100

    l8

---
+++ React/l8/src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Wrapper from './components/Wrapper/Wrapper'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <Wrapper/>
+  )
+}
+
+export default App
