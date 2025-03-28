f80414b4a699f6c69b4d73d2bf39779ca7d9e614
AntonVyhovskyi <vygovskijanton1@gmail.com>
2025-03-28 21:45:04 +0100

    plus devtools to l16

--- React/l16/redux/index.tsx
+++ React/l16/redux/index.tsx
@@ -1,5 +1,6 @@
 import {Dispatch, legacy_createStore} from 'redux';
 import { counterReducer, InitialState } from './reducers/counter';
 import { CounterActions } from './actions/counter';
+import { composeWithDevTools } from '@redux-devtools/extension';
 
-export const store = legacy_createStore<InitialState, CounterActions, Dispatch<CounterActions>, ()=>InitialState>(counterReducer);
+export const store = legacy_createStore<InitialState, CounterActions, Dispatch<CounterActions>, ()=>InitialState>(counterReducer, composeWithDevTools());
